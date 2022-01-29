b50cdc1bf0b0a3fdee16ca0adb9dba48b420e289
Mikhail Kashkov <mikhail.kashkov@student.vcc.ca>
2022-01-28 20:44:53 -0800

    start

---
+++ Hacker_News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker_News
+//
+//  Created by Mikhail Kashkov on 2022-01-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker_News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker_News
+//
+//  Created by Mikhail Kashkov on 2022-01-28.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
